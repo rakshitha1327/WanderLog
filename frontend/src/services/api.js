70d8860f1ffa3f3bebdbe2218ad7d9f798f23a0c
rakshitha1327 <rakshitha1327@gmail.com>
2026-02-11 15:02:31 +0530

    Updated frontend to use production backend URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://wanderlog-backend-3nod.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
